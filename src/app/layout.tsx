a09226551a39c883270c90dc67bdfb7cb74f7cee
sahebbali <saheb1996jmr@gmail.com>
2025-03-01 14:56:20 +0600

    add header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 import { Inter } from 'next/font/google'
 import "./globals.css"
 import Header from "@/components/Header"
-import Sidebar from "@/components/Sidebar"
+// import Sidebar from "@/components/Sidebar"
 import 'leaflet/dist/leaflet.css'
 import { Toaster } from 'react-hot-toast'
 import { getAvailableRewards, getUserByEmail } from '@/utils/db/actions'
@@ -47,7 +47,7 @@ export default function RootLayout({
         <div className="min-h-screen bg-gray-50 flex flex-col">
           <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} totalEarnings={totalEarnings} />
           <div className="flex flex-1">
-            <Sidebar open={sidebarOpen} />
+            {/* <Sidebar open={sidebarOpen} /> */}
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
               {children}
             </main>
