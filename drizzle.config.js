e3d8db83a9b3ea9b171086930142a4780625cee8
sahebbali <saheb1996jmr@gmail.com>
2025-03-01 14:35:55 +0600

    add layout

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,6 @@
 export default {
     dialect: "postgresql",
-    schema: "./src/utils/db/schema.ts",
+    schema: "./utils/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
       url: "postgresql://neondb_owner:npg_Hnoeh0SXl9VB@ep-misty-breeze-a8ebrgbt-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
